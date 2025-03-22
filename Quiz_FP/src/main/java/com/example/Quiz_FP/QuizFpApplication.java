220c0ee40d9af8abdb69f6b2d731139095ac8a77
komalpreet12730 <komalpreet12730@gmail.com>
2025-03-22 22:14:03 +1300

    start project

---
+++ Quiz_FP/src/main/java/com/example/Quiz_FP/QuizFpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quiz_FP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizFpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizFpApplication.class, args);
+	}
+
+}
---
+++ Quiz_FP/src/test/java/com/example/Quiz_FP/QuizFpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quiz_FP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizFpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
